d828ac1ecbd0557d02c0e2a7e4693b50d83b0e9d
RomanStashuk <romanstashuk1@gmail.com>
2023-08-07 19:53:51 +0300

    Remove unused methods & add id fiels

--- src/js/components/task.js
+++ src/js/components/task.js
@@ -1,18 +1,9 @@
 class Task {
+  id = Date.now().toString().slice(-10);
+
   constructor(description, completed) {
     this.description = description;
     this.completed = completed;
-  }
-
-  createHtml() {
-    return `
-      <li class="task">
-        <button class="task__btn  task__btn--${this.completed ? 'not-' : ''}done  btn"
-      aria-label="The task ${this.completed ? 'is' : 'not'}completed"></button>
-        <p class="task__text">${this.description}</p>
-        <button class="task__btn  task__btn--delete  btn" aria-label="Delete task"></button>
-      </li>
-    `;
   }
 }
 
